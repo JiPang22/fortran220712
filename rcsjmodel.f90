adca1c9f0a3f79a86f26b07ade91ed26ebb5c4fb
jp <jp@DESKTOP-TU2KPNK.localdomain>
2022-08-09 20:53:15 +0900

    a

--- rcsjmodel.f90
+++ rcsjmodel.f90
@@ -1,28 +1,25 @@
 program rcsj_model
-
 implicit none
+integer*4 m,k,kmax
+real*4 alpha,r,c,t,dt,phi,dphi,om,dom,flux,eta,tau,ic,dtau
+ic=100.e-6;t=0.;dt=1.e-2;phi=0.;om=0.;flux=2.e-15;r=1.e3;c=1.e-15
+eta=0.1;alpha=1/(10*acos(-1.));dtau=dt/(((flux)/(2*acos(-1.)*ic*r))
 
-integer*8 j,k
-
-real*8 t,dt,ph,dph,phdot,dphdot,flux,r,c,i,ic
-
-
-t=0.;dt=0.001;ph=0.1;phdot=0.1;flux=2.e-15;r=1.e3;c=1.e-4;i=0.0001;ic=0.0001
-
-open(1,file='plot')
-
-
-do k=0,1000
-
-write(1,*) (flux/6.28)*phdot,i 
-
-write(*,*) (flux/6.28)*phdot,i
-
-dph=phdot;dphdot=-(1/r*c)*phdot+(6.28/c*flux)*(i*j-ic*sin(ph))
-
-t=t+dt;ph=ph+dph*dt;phdot=phdot+dphdot*dt
-
+open(1,file='iv')
+open(2,file='it')
+do m=1,10
+do k=1,kmax
+eta=0.1*m
+sumv=sumv+dtau*(flux*om)/(2*acos(-1.)*1.e-3)
+write(2,*) tau,eta
+dom=eta-sin(phi)-alpha*om
+dphi=om
+om=om+dom*dtau
+phi=phi+dphi*dtau
 enddo
-
+write(1,*) sumv/(kmax*dtau),eta
+write(1,*) ''
+write(2,*) ''
+enddo
 
 end
