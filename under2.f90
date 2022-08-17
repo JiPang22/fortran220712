f09585819115f741de041fb8d50b64679ad063fb
root <root@jpbook.localdomain>
2022-08-17 20:11:07 +0900

    a

--- under2.f90
+++ under2.f90
@@ -1,31 +1,18 @@
 program underdynamic
 implicit none
-integer*4 i,j
-real*4 tau,dtau,ph,dph,om,dom,eta,beta
-dtau=1.e-2;beta=1.e-3
+integer i,j
+real dtau,ph,dph,om,dom,eta,beta
+dtau=1.e-2;beta=0.5
 open(1,file='aa')
 
-do j=26,30
+do j=1,5
 ph=1.;om=1.
 eta=0.2*j
-do i=0,0
+do i=1,12500
 write(1,*) i*dtau,om
-!dom=eta/beta -sin(ph)/beta -om/beta
+dom=-(1/beta)*om-(1/beta)*sin(ph)+eta
+om=om+dom*dtau
 dph=om
-om=eta-sin(ph)
-ph=ph+dph*dtau
-enddo
-write(1,*) ''
-enddo
-
-do j=6,10
-ph=1.;om=1.
-eta=0.2*j
-do i=1,2000
-write(1,*) i*dtau,om
-!dom=eta/beta -sin(ph)/beta -om/beta
-dph=om
-om=eta-sin(ph)
 ph=ph+dph*dtau
 enddo
 write(1,*) ''
